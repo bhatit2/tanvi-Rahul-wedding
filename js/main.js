0f0c67e77e1aefcb4a97e9155098d4a9f223139f
tbhatia1912 <tbhatia@tibco.com>
2018-10-01 23:14:17 +0530

    added event and venue sections

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var app = angular.module('myWeddingApp', []);
+app.controller('myController', function($scope, $http){
+    $scope.bride = 'Tanvi Bhatia';
+    $scope.groom = 'Rahul Raviprasad';
+});
