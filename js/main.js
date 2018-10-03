44897be9aa25c7665f07e7ceba49d4992b827fcd
tbhatia1912 <tbhatia@tibco.com>
2018-10-03 14:03:16 +0530

    added scroll action for options

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ var app = angular.module('myWeddingApp', []);
 app.controller('myController', function($scope, $http){
     $scope.bride = 'Tanvi Bhatia';
     $scope.groom = 'Rahul Raviprasad';
+    $scope.scrollToElement = function(id){
+        document.getElementById(id).scrollIntoView({
+            behavior: 'smooth'
+          });
+    }
 });
